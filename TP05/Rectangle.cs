a36db216a6fcba6653c22a34c2dd1055cb3bec29
Yoan Ballesteros <b.yoan@live.fr>
2016-10-04 19:36:21 +0200

    Création de la classe Rectangle et implémentation de ses méthodes

---
+++ TP05/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05
+{
+    class Rectangle : Figure
+    {
+        double x, y, longueur, largeur, surface;
+
+        public Rectangle(double x, double y, double longueur, double largeur) : base(x, y)
+        {
+            this.x = x;
+            this.y = y;
+            this.longueur = longueur;
+            this.largeur = largeur;
+        }
+
+        public double Surface()
+        {
+            this.surface = this.longueur * this.largeur;
+            return this.surface;
+        }
+    }
+}
