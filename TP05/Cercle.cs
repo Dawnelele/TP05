b1f7c5aa61c2bd00bd9c24e9d2c42efac5ced051
Yoan Ballesteros <b.yoan@live.fr>
2016-10-04 19:50:20 +0200

    Création de la classe Cercle et implémentation de ses méthodes

---
+++ TP05/Cercle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05
+{
+    class Cercle : Figure
+    {
+        double x, y, rayon, surface;
+        public Cercle(double x, double y, double rayon) : base(x, y)
+        {
+            this.x = x;
+            this.y = y;
+            this.rayon = rayon;
+        }
+
+        public double Surface()
+        {
+            this.surface = Math.PI * Math.Pow(this.rayon, 2);
+            return this.surface;
+        }
+    }
+}
