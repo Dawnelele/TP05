077081de3910caa9cdcb6eba28f66d81c71f1709
Yoan Ballesteros <b.yoan@live.fr>
2016-10-04 19:22:35 +0200

    Ajout de la classe Figure, et implémentations de ses méthodes

---
+++ TP05/Figure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05
+{
+    public class Figure
+    {
+        double x, y, surface;
+
+        public Figure(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Surface()
+        {
+            return this.surface;
+        }
+
+        public string Informations()
+        {
+            return "Les coordonnées du centre de la figure sont : x = " + this.x + " et y = " + this.y;
+        }
+    }
+}
